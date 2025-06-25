e07aab35eea5dc46b4cffca26520f681428a6eb2
mohammadamzad38 <hossainamzad38@gmail.com>
2025-06-25 15:47:36 +0600

    Header is completed without sub category

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,11 @@
 
 import Image from "next/image";
 import Link from "next/link";
+
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+
+import { IoCloseSharp, IoMenu } from "react-icons/io5";
 
 const NavLinks = [
   {
@@ -42,26 +46,30 @@ const NavLinks = [
 const Header = () => {
   const currentPath = usePathname();
 
+  const [open, setOpen] = useState(false);
+
+  function handleOpen() {
+    setOpen(!open);
+  }
+
   return (
-    <header className="bg-[#EEEEEE] sticky p-5 px-35">
-      <div className="flex justify-between items-center text-center max-w-[1200px] m-auto">
-        <div>
-          <a href="/">
-            {" "}
-            <Image
-              src="/Image/logo-z3-corporation.png"
-              alt="logo"
-              width={250}
-              height={250}
-            ></Image>
-          </a>
-        </div>
-        <div>
+    <header className="bg-[#EEEEEE] flex flex-col border-b ">
+      <div className="flex justify-between lg:gap-32 items-center max-w-[1200px] py-6 mx-auto">
+        <Link href="/">
+          <Image
+            src="/Image/logo-z3-corporation.png"
+            alt="logo"
+            width={250}
+            height={250}
+          ></Image>
+        </Link>
+
+        <div className="hidden md:flex items-center">
           {NavLinks.map((nav) => {
             const isActive = currentPath === nav.href;
             return (
               <Link
-                className={`mx-2 text-[#0A2C7D] text-sm font-bold ${
+                className={`mx-2 text-[#0A2C7D] hidden lg:block text-xs font-bold ${
                   isActive
                     ? "bg-[#F26F21] rounded-md px-2 py-2 text-white"
                     : "hover:text-[#F26F21]"
@@ -74,7 +82,43 @@ const Header = () => {
             );
           })}
         </div>
+
+        {/* Mobile Designe */}
+
+        <div
+          className="lg:hidden cursor-pointer ml-20 md:ml-80 text-4xl"
+          onClick={handleOpen}
+        >
+          <span className="text-gray-400 text-sm font-bold">MENU</span>
+          {open ? (
+            <IoCloseSharp className="text-black" />
+          ) : (
+            <IoMenu className="text-black" />
+          )}
+        </div>
       </div>
+
+      {open && (
+        <div className="flex flex-col border border-gray-950 lg:hidden  py-4 px-10 space-y-2">
+          {NavLinks.map((nav) => {
+            const isActive = currentPath === nav.href;
+            return (
+              <Link
+                key={nav.name}
+                href={nav.href}
+                onClick={() => setOpen(false)}
+                className={`text-[#0A2C7D] w-full font-bold ${
+                  isActive
+                    ? "bg-[#F26F21] text-white max-w-[50%] px-2 py-1 rounded-md"
+                    : "hover:text-[#F26F21]"
+                }`}
+              >
+                {nav.name}
+              </Link>
+            );
+          })}
+        </div>
+      )}
     </header>
   );
 };

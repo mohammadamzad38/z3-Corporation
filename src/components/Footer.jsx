7670aa6c3ad07dbd9a50c09024fb61b29ec5acb3
mohammadamzad38 <hossainamzad38@gmail.com>
2025-06-27 10:55:06 +0600

    footer is updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ import {
 const Footer = () => {
   return (
     <footer className="bg-[#383838] ">
-      <div className="max-w-[1200px] px-10 mx-auto py-20">
+      <div className="max-w-[1200px] px-10 mx-auto pt-20 pb-10">
         <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 lg:gap-16 gap-10">
           <div className="space-y-4">
             <Link href="/">
@@ -29,21 +29,21 @@ const Footer = () => {
             </p>
             <div className="flex gap-5">
               <Link
-                className="bg-[#0866FF] p-1 border border-[#0866FF] rounded-2xl"
+                className="bg-[#0866FF] hover:-translate-y-1 transform transition duration-200 ease-in-out p-1 border border-[#0866FF] rounded-2xl"
                 href="#"
               >
                 <FaSquareFacebook />
               </Link>
 
               <Link
-                className="bg-black p-1 border border-black rounded-2xl"
+                className="bg-black hover:-translate-y-1 transform transition duration-200 ease-in-out p-1 border border-black rounded-2xl"
                 href="#"
               >
                 <FaSquareXTwitter />
               </Link>
 
               <Link
-                className="bg-[#0A66C2] p-1 border border-[#0A66C2] rounded-2xl"
+                className="bg-[#0A66C2] hover:-translate-y-1 transform transition duration-200 ease-in-out p-1 border border-[#0A66C2] rounded-2xl"
                 href="#"
               >
                 <FaLinkedinIn />
@@ -66,9 +66,15 @@ const Footer = () => {
           <div>
             <h1 className="font-bold">QUICK LINKS</h1>
             <div className="text-sm flex flex-col space-y-2 lg:mt-14 mt-7">
-              <Link href="#">About Us</Link>
-              <Link href="#">Blog</Link>
-              <Link href="#">Contact Us</Link>
+              <Link className="hover:text-[#F26F21]" href="/about-us">
+                About Us
+              </Link>
+              <Link className="hover:text-[#F26F21]" href="/blog">
+                Blog
+              </Link>
+              <Link className="hover:text-[#F26F21]" href="/contact-us">
+                Contact Us
+              </Link>
             </div>
           </div>
 
@@ -105,7 +111,7 @@ const Footer = () => {
             CONTACT US
           </Link>
         </div>
-        <div className="text-center text-xs lg:mt-4 mt-7">
+        <div className="text-center text-sm lg:mt-4 mt-7">
           <h1>© 2019-2025 Z3 Corporation | All Rights Reserved.</h1>
         </div>
       </div>
